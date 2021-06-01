2dcef1cd73c2652229bfeb25070418bd45a8feaa
Jakubinka <contact@jakubkowalski.dev>
2021-06-01 20:40:17 +0200

    Co-authored-by: Radek <radekwawrzyk@gmail.com>

---
+++ src/components/WeatherCard/WeatherCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    xxdd
+  </div>
+</template>
+
+<script>
+import { onMounted, reactive, ref } from 'vue';
+
+export default {
+  name: 'WeatherCard',
+  props: {
+    weather: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+ 
+  },
+};
+</script>
+
+<style lang="scss" src="./WeatherCard.scss" />
